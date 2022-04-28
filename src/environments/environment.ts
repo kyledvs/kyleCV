7a7304bea943f6be88add537b41f1fc864f1bf9c
kyledvs <kyledvs58@gmail.com>
2022-04-28 16:08:40 +0200

    wakka

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'singularityk-6d3d8',
+    appId: '1:10121088179:web:9cfc4981822f0941dff4c6',
+    storageBucket: 'singularityk-6d3d8.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDc7q5QWW_Zg_WlYAx3QFs3uVj-zNBhlD4',
+    authDomain: 'singularityk-6d3d8.firebaseapp.com',
+    messagingSenderId: '10121088179',
+    measurementId: 'G-6GC775Q470',
+  },
   production: false
 };
 
